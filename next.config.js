d987c7d186fd57789ce916b124723754bee6e160
biz-yamakawa <105332545+biz-yamakawa@users.noreply.github.com>
2023-09-27 18:45:58 +0900

    orgの確認のため仮コミット

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   },
   poweredByHeader: false,
   experimental: { typedRoutes: true },
-  output: 'export',
+  // output: 'export',
 };
 
 module.exports = nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,13 +5,20 @@ import { Header } from 'src/app/_components/header';
 
 export const metadata = {
   title: 'profile',
+  openGraph: {
+    title: 'my profile',
+    description: 'プロフィールと職務経歴',
+    type: 'article',
+    siteName: 'profile',
+    images: 'http://www.bizreach.jp/img/logo/top-bizreach-ogp-img.png',
+  },
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang='en'>
       <body>
-        <div className='flex flex-col min-h-screen font-mono'>
+        <div className='flex min-h-screen flex-col font-mono'>
           <Header />
           {children}
           <Footer />
